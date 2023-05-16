82b62b808b2b492cfe03293fbe6799d70f87afc8
SimonVillalonIT <simonvillallon10@gmail.com>
2023-05-16 20:16:08 -0300

    Tweaks to the page

--- Package/Device/sketch_may16a/sketch_may16a.ino
+++ Package/Device/sketch_may16a/sketch_may16a.ino
@@ -7,20 +7,123 @@ const char* password = "Password1";  // Contraseña de la red WiFi del punto de 
 
 AsyncWebServer server(80);
 
-const char* htmlContent = R"(
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Formulario</title>
-</head>
-<body>
-  <h1>Formulario</h1>
-  <form action="/form" method="post">
-    <input type="text" name="input_name" placeholder="Nombre">
-    <input type="submit" value="Enviar">
-  </form>
-</body>
+const char* htmlContent = R"(<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Configurar Red</title>
+    <style>
+      * {
+        margin: 0;
+        padding: 0%;
+        font-family: "poppins", sans-serif;
+      }
+      section {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        min-height: 100vh;
+        width: 100%;
+        background: linear-gradient(
+          to right,
+          rgb(55, 65, 81),
+          rgb(17, 24, 39),
+          rgb(0, 0, 0)
+        );
+        background-position: center;
+        background-size: cover;
+      }
+      .form-box {
+        position: relative;
+        width: 400px;
+        height: 450px;
+        background: transparent;
+        border: 2px solid rgba(255, 255, 255, 0.5);
+        border-radius: 20px;
+        backdrop-filter: blur(15px);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+      h2 {
+        font-size: 2em;
+        color: #fff;
+        text-align: center;
+      }
+      .inputbox {
+        position: relative;
+        margin: 30px 0;
+        width: 310px;
+        border-bottom: 2px solid #fff;
+      }
+      .inputbox label {
+        position: absolute;
+        top: 50%;
+        left: 5px;
+        transform: translateY(-50%);
+        color: #fff;
+        font-size: 1em;
+        pointer-events: none;
+        transition: 0.5s;
+      }
+      input:focus ~ label,
+      input:focus ~ :valid {
+        top: -5px;
+      }
+      .inputbox input {
+        width: 100%;
+        height: 50px;
+        background: transparent;
+        border: none;
+        outline: none;
+        font-size: 1em;
+        padding: 0 35px 0 5px;
+        color: #fff;
+      }
+      .inputbox ion-icon {
+        position: absolute;
+        right: 8px;
+        color: #fff;
+        font-size: 1.2em;
+        top: 20px;
+      }
+      button {
+        width: 100%;
+        height: 40px;
+        border-radius: 40px;
+        background: #fff;
+        border: none;
+        outline: none;
+        cursor: pointer;
+        font-size: 1em;
+        font-weight: 600;
+      }
+    </style>
+  </head>
+  <body>
+    <section>
+      <div class="form-box">
+        <div class="form-value">
+          <form action="">
+            <h2>Configurar red</h2>
+            <div class="inputbox">
+              <input type="text" name="ssid" required />
+              <label for="">SSID</label>
+            </div>
+            <div class="inputbox">
+              <input type="password" name="password" required />
+              <label for="">Contraseña</label>
+            </div>
+            <button>Enviar</button>
+          </form>
+        </div>
+      </div>
+    </section>
+  </body>
 </html>
+
 )";
 
 void setup() {
