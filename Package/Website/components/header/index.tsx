ad5736d93822d1589808b85cd8bb93f77cc28cba
SimonVillalonIT <simonvillalon10@gmail.com>
2023-08-24 20:05:06 -0300

    Added next themes

--- Package/Website/components/header/index.tsx
+++ Package/Website/components/header/index.tsx
@@ -1,13 +1,12 @@
 import Link from "./link";
-import ToggleTheme from "./toggle-theme";
 import { home } from "data";
 import HeaderContainer from "./header-container";
 
 export default function Header() {
   return (
     <HeaderContainer>
-      {home.header.links.map((link) => (
-        <Link {...link} />
+      {home.header.links.map((link, i) => (
+        <Link key={i} {...link} />
       ))}
     </HeaderContainer>
   );
